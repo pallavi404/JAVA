153532c0c78c774e5803944d653afdce2e5ec5fc
Pallavi Jain <jainpallavi717@gmail.com>
2026-02-27 17:31:06 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,11 @@
+public class condition{
+    public static void main(String[] args){
+        int x=2;
+        if(x == 2){
+            System.out.println("hello");
+        }
+        else{
+            System.out.println("hi");
+        }
+    }
+}
